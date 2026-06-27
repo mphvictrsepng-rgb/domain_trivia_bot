5cd005f6420a3e9a9e6c58407032f0a36649e34a
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-06-27 19:07:01 +0200

    did another bot just to practice the same concept more until it sticks

---
+++ banking_bot.js
@@ -0,0 +1,26 @@
+console.log('Hello! I\'m your banking buddy!');
+const botName = 'Zaka';
+const botLocation = 'Sepeng Holdings Pty';
+const botService = 'Wealth management';
+
+console.log('My name is ' + botName + ' , and I\'m from ' + botLocation + '.');
+console.log('I will be providing you with ' + botService + ' solutions.');
+
+console.log('='.repeat(60));
+
+let bankingSolution = botService + ' starts with maximizing your R36,000 annual and R500,000 lifetime contributions. Use 100% equity-focused funds here to eliminate Capital Gains Tax (CGT) and dividend withholding tax permanently.';
+console.log('Solution one: ' + bankingSolution);
+
+console.log('='.repeat(60));
+
+bankingSolution = 'Here at ' + botLocation + ' we can help you to maximize contributions to a Retirement Annuity (RA) or workplace pension up to 27.5% of your taxable income (capped at R350,000 annually).';
+console.log('Solution two: ' + bankingSolution);
+
+console.log('='.repeat(60));
+
+bankingSolution = 'Unlike other institutions, ' + botLocation + ' will not lock your capital into fixed periods or penalize you for changing your contribution amounts.';
+console.log('Solution three: ' + bankingSolution);
+
+console.log('='.repeat(60));
+
+console.log('It was nice sharing these banking solutions with you. For more clarity, feel free to ask more questions. Goodbye! - ' + botName + ' from ' + botLocation);
