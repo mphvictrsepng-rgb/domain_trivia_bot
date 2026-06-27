15b2bd07f47996c47ccf1e722270b1c3702360c0
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-06-27 14:37:29 +0200

    completed exercise

---
+++ trivia_bot.js
@@ -0,0 +1,24 @@
+console.log('Hello! I am your coding fun fact guide!');
+const botName = 'Prime';
+const botLocation = 'Wakanda';
+const botFavoriteLanguage = 'Python';
+console.log('My name is ' + botName + ' and I live on ' + botLocation + '.');
+console.log('My favorite programming language is ' + botFavoriteLanguage + '.');
+
+console.log('.'.repeat(55))
+
+let codingFact = 'NASA uses ' + botFavoriteLanguage + ' extensively on the Mars rovers.';
+console.log('1st Coding fact: ' + codingFact);
+
+console.log('.'.repeat(55))
+
+codingFact = 'Here is another cool fact, if you type import antigravity into a ' + botFavoriteLanguage + ' interpreter, it opens a web browser displaying a classic xkcd comic about the language.';
+console.log('2nd Coding fact: ' + codingFact);
+
+console.log('.'.repeat(55))
+
+codingFact = botFavoriteLanguage + " is actually older than Java, with its first version released in 1991, compared to Java's release in 1995.";
+console.log('3rd Coding fact: ' + codingFact);
+
+console.log('.'.repeat(55))
+console.log('It was fun sharing these facts with you. Goodbye! - ' + botName + ' from ' + botLocation);
